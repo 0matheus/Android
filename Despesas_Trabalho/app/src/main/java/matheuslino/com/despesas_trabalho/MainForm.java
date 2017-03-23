5209a15f2c1a89eb096cec9e99eccd0bd39562a3
Matheus Lino <0matheus.c.l@gmail.com>
2017-03-23 09:51:45 -0300

    Melhorando app

--- Despesas_Trabalho/app/src/main/java/matheuslino/com/despesas_trabalho/MainForm.java
+++ Despesas_Trabalho/app/src/main/java/matheuslino/com/despesas_trabalho/MainForm.java
@@ -1,7 +1,10 @@
 package matheuslino.com.despesas_trabalho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainForm extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainForm extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_form);
+
+        Button btnCalcular = (Button) findViewById(R.id.btnCalcular);
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent ResumeIntent = new Intent(MainForm.this, Resume.class);
+                startActivity(ResumeIntent);
+            }
+        });
     }
 }
---
+++ Despesas_Trabalho/app/src/main/java/matheuslino/com/despesas_trabalho/entity/Despesa.java
@@ -0,0 +1,27 @@
+package matheuslino.com.despesas_trabalho.entity;
+
+/**
+ * Created by mathe on 23/03/2017.
+ */
+
+public class Despesa {
+
+    private int id;
+    private double valor;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+}
