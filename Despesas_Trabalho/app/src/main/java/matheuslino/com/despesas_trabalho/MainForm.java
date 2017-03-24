eda4228cdaa5f9b6f6bfdcbdbcc52dc34cde39dd
Matheus Lino <0matheus.c.l@gmail.com>
2017-03-24 00:09:05 -0300

    Criando tela 2 e devidos Intents

--- Despesas_Trabalho/app/src/main/java/matheuslino/com/despesas_trabalho/MainForm.java
+++ Despesas_Trabalho/app/src/main/java/matheuslino/com/despesas_trabalho/MainForm.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainForm extends AppCompatActivity {
 
@@ -18,6 +19,18 @@ public class MainForm extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent ResumeIntent = new Intent(MainForm.this, Resume.class);
+                Bundle b = new Bundle();
+                EditText nomeETxt = (EditText) findViewById(R.id.nCliente);
+                EditText cidadeETxt = (EditText) findViewById(R.id.cCliente);
+                EditText despesaETxt = (EditText) findViewById(R.id.vDespesa);
+
+                b.putString("NOME", nomeETxt.getText().toString());
+                b.putString("CIDADE", cidadeETxt.getText().toString());
+
+                Double despesa = Double.parseDouble(despesaETxt.getText().toString());
+                b.putDouble("DESPESA", despesa);
+
+                ResumeIntent.putExtras(b);
                 startActivity(ResumeIntent);
             }
         });
--- Despesas_Trabalho/app/src/main/java/matheuslino/com/despesas_trabalho/Resume.java
+++ Despesas_Trabalho/app/src/main/java/matheuslino/com/despesas_trabalho/Resume.java
@@ -1,13 +1,63 @@
 package matheuslino.com.despesas_trabalho;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Resume extends AppCompatActivity {
+
+    public Double desconto = 0.0;
+    public Double valorLiquido = 0.0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resume);
+
+        Button btnVoltar = (Button) findViewById(R.id.btnVoltar);
+        btnVoltar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Resume.this, MainForm.class);
+                startActivity(i);
+
+            }
+        });
+
+        Intent iResume = getIntent();
+        Bundle bResume = iResume.getExtras();
+        Double despesa = bResume.getDouble("DESPESA");
+        calcularDesconto(despesa);
+        String nome = bResume.getString("NOME");
+        String cidade = bResume.getString("CIDADE");
+        TextView txtNome = (TextView) findViewById(R.id.txt_ResumeCliente);
+        txtNome.setText(nome);
+        TextView txtCidade = (TextView) findViewById(R.id.txt_ResumeCidade);
+        txtCidade.setText(cidade);
+        TextView txtDespesa = (TextView) findViewById(R.id.txt_ResumeDespesa);
+        txtDespesa.setText(despesa.toString());
+        TextView txtDesconto = (TextView) findViewById(R.id.txt_ResumeDesconto);
+        txtDesconto.setText(desconto.toString());
+        TextView txtValorL = (TextView) findViewById(R.id.txt_ResumeValorLiquido);
+        txtValorL.setText(valorLiquido.toString());
+
+
     }
+
+    private void calcularDesconto(Double despesa) {
+        if (despesa <= 500) {
+            desconto = 0.05;
+            valorLiquido = despesa - (despesa * desconto);
+        } else if (despesa < 1000) {
+            desconto = 0.10;
+            valorLiquido = despesa - (despesa * desconto);
+        } else {
+            desconto = 0.15;
+            valorLiquido = despesa - (despesa * desconto);
+        }
+    }
+
 }
